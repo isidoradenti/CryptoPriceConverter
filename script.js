57490e0d4b93a1b347a5464396385499bbcae894
Adam <123258545+isidoradenti@users.noreply.github.com>
2023-12-18 13:59:13 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function getCryptoPrice() {
+    const inputAmount = document.getElementById("cryptoInput").value;
+
+    if (!inputAmount || isNaN(inputAmount)) {
+        alert("Please enter a valid amount.");
+        return;
+    }
+
+    const apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const bitcoinPrice = data.bitcoin.usd;
+            const convertedAmount = inputAmount * bitcoinPrice;
+            const resultText = `${inputAmount} USD is approximately ${convertedAmount.toFixed(2)} BTC`;
+            document.getElementById("cryptoResult").innerText = resultText;
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            alert("Error fetching data. Please try again later.");
+        });
+}
